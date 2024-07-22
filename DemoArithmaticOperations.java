55d97f26f83255ec264f826ce3132cde9c31d749
Karthik-btech <karthikrock921@gmail.com>
2024-07-22 15:09:59 +0530

    JAVA
    FIRST LAB

---
+++ DemoArithmaticOperations.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class DemoArithmaticOperations
+{
+	public static void main(String k[])
+	{
+		int a=100,b=20;
+		int res=a-b;
+		System.out.println("Result="+res);
+	}
+}
---
+++ firstpro.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+class firstpro
+{
+static public void main(String k[])
+{
+System.out.println("hello karthik");
+System.out.println("3+2");
+System.out.println(3+2);
+System.out.println(3>=2);
+}
+}
